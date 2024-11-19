992c591a1c01fac7c77b0ec741f514fc22ce328c
Relict <vostryakovkirill@gmail.com>
2024-11-19 22:36:25 +0400

    Initial commit

---
+++ src/main/java/ru/netology/FormDate.java
@@ -0,0 +1,7 @@
+package ru.netology;
+
+public class FormDate {
+    public int day; // день рождения
+    public int month; // месяц рождения
+    public int year; // год рождения
+}
---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,11 @@
+package ru.netology;
+
+public class Post {
+    public String name; // поле для имени
+    public String passport; // поле для паспорта
+    public String patronymic; // поле для отчества
+    public String phone; // поле для телефона
+    public String surname; // поле для фамилии
+    public boolean subscription; // поле для подписки
+    public FormDate birthday; // поле для даты рождения
+}
